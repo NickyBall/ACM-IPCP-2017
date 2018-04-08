5c95c7211aee7cab3ee0191fb8f19f58921487c3
Jakkrit Junrat <ball.vong.yo.gh@gmail.com>
2018-04-09 00:16:54 +0700

    Question A

---
+++ ACM-ICPC-2017-A/FileManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ACM_ICPC_2017_A
+{
+    public class FileManager
+    {
+        public List<string> GetFileInFolder(string QuestionFolder) => Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\Files\\{QuestionFolder.ToUpper()}").Where(f => Regex.IsMatch(f, @"sample-\d.in")).ToList();
+
+        public List<string> GetContentFromFile(string FilePath)
+        {
+            List<string> Lines = new List<string>();
+            using (StreamReader reader = new StreamReader(FilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    Lines.Add(reader.ReadLine());
+                }
+            }
+            return Lines;
+        }
+    }
+}
---
+++ ACM-ICPC-2017-A/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM_ICPC_2017_A
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public double DistanceFrom(Point Target) => Math.Sqrt(Math.Pow(X - Target.X, 2) + Math.Pow(Y - Target.Y, 2));
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            QuestionA();
+            
+        }
+        
+
+        static void QuestionA()
+        {
+            FileManager manager = new FileManager();
+            var Files = manager.GetFileInFolder("A");
+            foreach (var file in Files)
+            {
+                var StopWatch = new Stopwatch();
+                StopWatch.Start();
+                var contents = manager.GetContentFromFile(file);
+                int NumberOfLine = Int16.Parse(contents[0]);
+                double MaxDistance = 0;
+                for (int i = 1; i <= NumberOfLine; i++)
+                {
+                    var BeginPoint = contents[i].Split(' ');
+                    Point Beginning = new Point()
+                    {
+                        X = Int16.Parse(BeginPoint[0]),
+                        Y = Int16.Parse(BeginPoint[1])
+                    };
+                    for (int j = 1; j < i; j++)
+                    {
+                        var Endpoint = contents[j].Split(' ');
+                        Point Ending = new Point()
+                        {
+                            X = Int16.Parse(Endpoint[0]),
+                            Y = Int16.Parse(Endpoint[1])
+                        };
+                        double Distance = Ending.DistanceFrom(Beginning);
+                        if (Distance > MaxDistance) MaxDistance = Distance;
+                    }
+                }
+                Console.WriteLine($"Distance for {file} is {MaxDistance}");
+                StopWatch.Stop();
+                Console.WriteLine($"Elapsed Time {StopWatch.ElapsedMilliseconds}");
+            }
+        }
+        static void QuestionB()
+        {
+
+        }
+    }
+}
